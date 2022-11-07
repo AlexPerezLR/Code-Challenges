51e95fffc7935a5c0dbbb8fa1ba76891cd3e4d1e
Alejandro <alejandroperez.info@gmail.com>
2022-11-07 21:20:28 +0100

    Fix

--- Retos/src/boomerang/Boomerang.java
+++ Retos/src/boomerang/Boomerang.java
@@ -2,7 +2,7 @@ package boomerang;
 
 public class Boomerang {
     public static void main(String[] args){
-        System.out.println(countBoomerangV2(new int[]{2,1,2,3,5,4,5,4}));
+        System.out.println(countBoomerangV2(new int[]{2,2,2,3,5,4,5,4}));
     }
 
     public static int countBoomerangV2(int[] nums){
@@ -10,14 +10,15 @@ public class Boomerang {
         int times = 0;
         int odds = -1;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]%2==0){
-                if(evens==nums[i]){
+
+            if(i%2==0){
+                if(evens==nums[i] && nums[i]!=odds){
                     times++;
                 }else{
                     evens = nums[i];
                 }
             }else{
-                if(odds==nums[i]){
+                if(odds==nums[i] && nums[i]!=evens){
                     times++;
                 }else{
                     odds = nums[i];
